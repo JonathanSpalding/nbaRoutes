2b5110bba6662f6528fc4f646aa3bdf27e490081
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-05-11 17:25:42 -0600

    Trying out routing on gh-pages

---
+++ js/teams/teamService.js
@@ -0,0 +1,14 @@
+var app = angular.module('nbaRoutes');
+
+app.service('teamService', function($http, $q){
+    this.gameObj = function(){
+        $http({
+            method: url
+        :
+        'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+    })
+    }
+
+
+});
+
